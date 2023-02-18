b890f8a6cde2f234b2fef0e0a9421f9f609047b3
draltsevich <Dzmitry.Raltsevich@coxautoinc.com>
2023-02-18 12:21:06 +0300

    Add one more test

--- src/test/java/JUnit5ExampleTests.java
+++ src/test/java/JUnit5ExampleTests.java
@@ -3,21 +3,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import static io.restassured.RestAssured.given;
 
 @DisplayName("Class With Tests")
 public class JUnit5ExampleTests {
 
     @Test
-    @DisplayName("First test")
-    void justSimpleTest() {
+    @DisplayName("Check QA User")
+    void Check_QA_User() {
         RequestSpecification authentificateSpec = given()
                 .baseUri("https://qa.flex.cafe/api/v1/Auth/AuthenticateUser")
                 .header("Flex-Host", "qa.flex.cafe")
+                .header("Content-Type", "application/json")
+                .body(String.format("{\"username\": \"%s\", \"password\": \"%s\"}", "categories_trusted", "Password1!"));
+
+        var response = authentificateSpec
+                .post()
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+
+        var token = response.jsonPath().getString("token");
+        var id = response.jsonPath().get("session.user.id");
+
+        System.out.println(token);
+    }
+
+    @Test
+    @DisplayName("Check UAT User")
+    void Check_UAT_User() {
+        RequestSpecification authentificateSpec = given()
+                .baseUri("https://uat.flex.cafe/api/v1/Auth/AuthenticateUser")
+                .header("Flex-Host", "uat.flex.cafe")
                 .header("Content-Type", "application/json")
                 .body(String.format("{\"username\": \"%s\", \"password\": \"%s\"}", "categories_trusted", "Password1!"));
 
